49c19efdef4f28a0ffab9b3837389727d343d8ce
Peter Vila <peterdvila@gmail.com>
2022-01-30 22:32:46 -0800

    Added button to delete our post using DELETE method. Added an alert to notify user the request got resolved. Added possible error handler for wrong endpoints or network error.

--- src/App.js
+++ src/App.js
@@ -7,10 +7,13 @@ const baseURL = "https://jsonplaceholder.typicode.com/posts";
 
 function App() {
   const [post, setPost] = useState();
+  const [error, setError] = useState();
 
   useEffect(() => {
     axios.get(`${baseURL}/1`).then((response) => {
       setPost(response.data);
+    }).catch(error => {
+      setError(error);
     });
   }, []) //empty [] as second argument to prevent rerender due to no dependencies
 
@@ -30,11 +33,22 @@ function App() {
       .put(`${baseURL}/1`, {
         title: "Hello World!",
         body: "This is an updated post.",
-      })
+      }) //JSON API successfully sends us our put request as a response
       .then((response) => {
         setPost(response.data);
       });
   }
+
+  const deletePost = () => {
+    axios.delete(`${baseURL}/1`).then(() => { //This then is not required, but ensures request was resolved
+      alert("Post deleted!");
+      setPost(null);
+    });
+  }
+
+  if (error) return `Error: ${error.message}`;
+  if (!post) return "No post!";
+
 
   return (
     <div className="App">
@@ -44,6 +58,7 @@ function App() {
           <p>{post.body}</p>
           <button onClick={createPost}>Create Post</button>
           <button onClick={updatePost}>Update Post</button>
+          <button onClick={deletePost}>Delete Post</button>
         </>
       )}
       <Input />
